09d552ca2c112663b63e508e84bc070fd90a3c9d
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2021-08-04 10:18:37 +0800

    update

--- label_generator_v3.py
+++ label_generator_v3.py
@@ -21,6 +21,8 @@ class LabelGeneratorV3(object):
         self.file3_path = os.path.join(DATA_DIR, "numbers_files", "3d30b85f-5318-4bbb-ab1b-7262ad74fd7c_166330.csv")
         self.file4_path = os.path.join(DATA_DIR, "numbers_files", "beab7dca-931a-4b67-ab16-02daa8f3199e_166322.csv")
         self.file5_path = os.path.join(DATA_DIR, "numbers_files", "clean_num_and_op_train.txt")
+        self.file6_path = os.path.join(DATA_DIR, "numbers_files", "e0588eb6-f3e9-41f7-86c8-a019e33c7a33_166355.csv")
+        self.file7_path = os.path.join(DATA_DIR, "numbers_files", "8ad26809-6a2c-48d6-83d3-7133ad7d236f_166331.csv")
         self.out_file_path = os.path.join(DATA_DIR, 'numbers_files', 'out-{}.txt'.format(get_current_time_str()))
 
     @staticmethod
@@ -101,7 +103,7 @@ class LabelGeneratorV3(object):
         处理单行数据
         """
         try:
-            new_img_name = "v1-{}-{}.jpg".format(get_current_day_str(), str(idx).zfill(6))
+            new_img_name = "v1_1-{}-{}.jpg".format(get_current_day_str(), str(idx).zfill(6))
             new_img_url = LabelGeneratorV3.format_url(img_url, new_img_name)
             new_img_label = LabelGeneratorV3.format_label(img_label)
             if not new_img_label:
@@ -110,26 +112,6 @@ class LabelGeneratorV3(object):
             print('[Info] 处理完成: {}'.format(idx))
         except Exception as e:
             print('[Info] 处理失败: {}'.format(idx))
-
-    def process(self):
-        print('[Info] file_path: {}'.format(self.file_path))
-        data = pandas.read_csv(self.file_path)
-        print("[Info] 样本数: {}".format(data.shape[0]))
-        num_error = 0
-        num_count = 0
-
-        pool = Pool(processes=100)
-        for idx, row in data.iterrows():
-            # print('-' * 100)
-            # LabelGeneratorV2.process_line(idx, row, self.out_file_path)
-            pool.apply_async(LabelGeneratorV3.process_line, (idx, row, self.out_file_path))
-            # num_count += 1
-            # if idx == 1000:
-            #     break
-        pool.close()
-        pool.join()
-        print('[Info] error: {}, count: {}, 错误率: {}'
-              .format(num_error, num_count, safe_div(num_error, num_count)))
 
     @staticmethod
     def process_file(file_path):
@@ -172,7 +154,7 @@ class LabelGeneratorV3(object):
         print('[Info] ' + "-" * 50)
         return image_label_dict
 
-    def process_v2(self):
+    def process_v1(self):
         """
         处理数据
         """
@@ -208,6 +190,38 @@ class LabelGeneratorV3(object):
 
         print('[Info] 处理完成: {}'.format(self.out_file_path))
 
+    def process_v1_1(self):
+        """
+        处理数据
+        """
+        image1_label_dict = LabelGeneratorV3.process_file(self.file6_path)
+        image2_label_dict = LabelGeneratorV3.process_file(self.file7_path)
+
+        res_label_dict = dict()
+        for image_x_label_dict in [image2_label_dict]:
+            for image_url in image_x_label_dict.keys():
+                if image_url not in image1_label_dict.keys():
+                    continue
+                label_ex1 = image1_label_dict[image_url]
+                label_ex2 = image_x_label_dict[image_url]
+                if label_ex1 != label_ex2:
+                    continue
+                else:
+                    res_label_dict[image_url] = label_ex1
+        print('[Info] 样本数: {}'.format(len(res_label_dict.keys())))
+        print('[Info] 样本数ex: {}'.format(len(image1_label_dict.keys())))
+
+        pool = Pool(processes=100)
+        for img_idx, img_url in enumerate(res_label_dict.keys()):
+            img_label = res_label_dict[img_url]
+            pool.apply_async(LabelGeneratorV3.process_line, (img_idx, img_url, img_label, self.out_file_path))
+            # LabelGeneratorV3.process_line(img_idx, img_url, img_label, self.out_file_path)
+            # if img_idx == 10:
+            #     break
+        pool.close()
+        pool.join()
+
+        print('[Info] 处理完成: {}'.format(self.out_file_path))
 
     @staticmethod
     def split_train_test():
@@ -226,7 +240,8 @@ class LabelGeneratorV3(object):
 
 def main():
     lg = LabelGeneratorV3()
-    lg.process_v2()
+    # lg.process_v1()
+    lg.process_v1_1()
     # lg.split_train_test()
 
 
