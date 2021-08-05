5f8ad47a930a3d24fbc403e6b7fd272592c75f33
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2021-08-05 15:50:05 +0800

    update

--- services_versus.py
+++ services_versus.py
@@ -18,9 +18,9 @@ class ServicesVersus(object):
     def __init__(self):
         # file_name = "a2e455ff-b77b-4f65-ae59-864cfa20bdd8_166274.out-20210803112232.vs-20210803150735"
         # file_name = "clean_num_and_op_test"
-        file_name = "clean_hw_numbers_v1_test"
+        file_name = "hw_numbers_check_3000"
         self.file_path = os.path.join(DATA_DIR, 'numbers_files', '{}.txt'.format(file_name))
-        self.out_file_path = os.path.join(DATA_DIR, '{}.vs-{}.txt'.format(file_name, get_current_time_str()))
+        self.out_file_path = os.path.join(DATA_DIR, 'numbers_files', '{}.vs-{}.txt'.format(file_name, get_current_time_str()))
 
     @staticmethod
     def predict_danjing(img_url):
@@ -41,15 +41,16 @@ class ServicesVersus(object):
         return ocr_text
 
     @staticmethod
-    def process_line(data_idx, img_url, out_file):
+    def process_line(data_idx, img_url, label_str, out_file):
         res1 = ServicesVersus.predict_danjing(img_url)
         res2 = ServicesVersus.predict_v1(img_url)
         res3 = ServicesVersus.predict_v1_1(img_url)
         # img_url = img_url.replace("http://quark-cv-data.oss-cn-hangzhou.aliyuncs.com",
         #                           "https://quark-cv-data.oss-cn-hangzhou.alibaba-inc.com")
-        if res1 != res2 or res2 != res3 or res1 != res3:
-            print('[Info] res1: {}, res2: {}, res3: {}, img_url: {}'.format(res1, res2, res3, img_url))
-            write_line(out_file, ",".join([res1, res2, res3, img_url]))
+        if res1 != label_str or res2 != label_str or res3 != label_str:
+            print('[Info] data_idx: {}, label_str: {}, res1: {}, res2: {}, res3: {}, img_url: {}'
+                  .format(data_idx, label_str, res1, res2, res3, img_url))
+            write_line(out_file, ",".join([label_str, res1, res2, res3, img_url]))
         if data_idx % 100 == 0:
             print('[Info] idx: {}'.format(data_idx))
 
@@ -60,8 +61,9 @@ class ServicesVersus(object):
         pool = Pool(processes=40)
         for data_idx, data_line in enumerate(data_lines):
             img_url = data_line.split("\t")[0]
-            # ServicesVersus.process_line(data_idx, img_url, self.out_file_path)
-            pool.apply_async(ServicesVersus.process_line, (data_idx, img_url, self.out_file_path))
+            label_str = data_line.split("\t")[1]
+            # ServicesVersus.process_line(data_idx, img_url, label_str, self.out_file_path)
+            pool.apply_async(ServicesVersus.process_line, (data_idx, img_url, label_str, self.out_file_path))
         pool.close()
         pool.join()
         print('[Info] 全部处理完成! {}'.format(self.out_file_path))
