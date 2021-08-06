1f2e78c79fd7375d3f8b2ef77bd2eb8272ab38bf
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2021-08-06 10:07:00 +0800

    update

--- services_versus.py
+++ services_versus.py
@@ -45,27 +45,27 @@ class ServicesVersus(object):
 
     @staticmethod
     def process_line(data_idx, img_url, label_str, out_file):
-        try:
-            # res1 = ServicesVersus.predict_danjing(img_url)
-            # res2 = ServicesVersus.predict_v1(img_url)
-            res3 = ServicesVersus.predict_v1_1(img_url)
-            # img_url = img_url.replace("http://quark-cv-data.oss-cn-hangzhou.aliyuncs.com",
-            #                           "https://quark-cv-data.oss-cn-hangzhou.alibaba-inc.com")
-            # if res1 != label_str or res2 != label_str or res3 != label_str:
-            #     print('[Info] data_idx: {}, label_str: {}, res1: {}, res2: {}, res3: {}, img_url: {}'
-            #           .format(data_idx, label_str, res1, res2, res3, img_url))
-            #     write_line(out_file, ",".join([label_str, res1, res2, res3, img_url]))
-            res = res3
-            if res != label_str:
-                img_url_show = img_url.replace("http://quark-cv-data.oss-cn-hangzhou.aliyuncs.com",
-                                               "https://quark-cv-data.oss-cn-hangzhou.alibaba-inc.com")
-                print('[Info] data_idx: {}, label_str: {}, res1: {}, img_url_show: {}'
-                      .format(data_idx, label_str, res, img_url_show))
-                write_line(out_file, ",".join([label_str, res, img_url]))
-            if data_idx % 100 == 0:
-                print('[Info] idx: {}'.format(data_idx))
-        except Exception as e:
-            print('[Error] data_idx: {}'.format(data_idx))
+        # try:
+        # res1 = ServicesVersus.predict_danjing(img_url)
+        # res2 = ServicesVersus.predict_v1(img_url)
+        res3 = ServicesVersus.predict_v1_1(img_url)
+        # img_url = img_url.replace("http://quark-cv-data.oss-cn-hangzhou.aliyuncs.com",
+        #                           "https://quark-cv-data.oss-cn-hangzhou.alibaba-inc.com")
+        # if res1 != label_str or res2 != label_str or res3 != label_str:
+        #     print('[Info] data_idx: {}, label_str: {}, res1: {}, res2: {}, res3: {}, img_url: {}'
+        #           .format(data_idx, label_str, res1, res2, res3, img_url))
+        #     write_line(out_file, ",".join([label_str, res1, res2, res3, img_url]))
+        res = res3
+        if res != label_str:
+            img_url_show = img_url.replace("http://quark-cv-data.oss-cn-hangzhou.aliyuncs.com",
+                                           "https://quark-cv-data.oss-cn-hangzhou.alibaba-inc.com")
+            print('[Info] data_idx: {}, label_str: {}, res1: {}, img_url_show: {}'
+                  .format(data_idx, label_str, res, img_url_show))
+            write_line(out_file, ",".join([label_str, res, img_url]))
+        if data_idx % 10 == 0:
+            print('[Info] idx: {}'.format(data_idx))
+        # except Exception as e:
+        #     print('[Error] data_idx: {}'.format(data_idx))
 
     def process(self):
         print('[Info] file_path: {}'.format(self.file_path))
@@ -75,8 +75,8 @@ class ServicesVersus(object):
         for data_idx, data_line in enumerate(data_lines):
             img_url = data_line.split("\t")[0]
             label_str = data_line.split("\t")[1]
-            # ServicesVersus.process_line(data_idx, img_url, label_str, self.out_file_path)
-            pool.apply_async(ServicesVersus.process_line, (data_idx, img_url, label_str, self.out_file_path))
+            ServicesVersus.process_line(data_idx, img_url, label_str, self.out_file_path)
+            # pool.apply_async(ServicesVersus.process_line, (data_idx, img_url, label_str, self.out_file_path))
         pool.close()
         pool.join()
         print('[Info] 全部处理完成! {}'.format(self.out_file_path))
